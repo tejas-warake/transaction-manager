d36f9e59e6311fa48a75d29e887a656514159e6b
tejas-warake <tswarake15@gmail.com>
2023-01-28 18:28:55 +0530

    commit2

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -38,7 +38,7 @@ export default function Login() {
             Cookie.set("token", token);
             dispatch(setUser(user));
             navigate("/");
-            // window.location.reload();
+            window.location.reload();
         }
     };
 
